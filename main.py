ad0b5f2871c6ce8b9ce048ee3f2c3ee1eb2fd3a9
DaikinFBN <109244363+DaikinFBN@users.noreply.github.com>
2023-01-09 10:51:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import os
 
-with open('variables.txt','w') as variable_file:
-    variable_file.writelines('False\nFalse')
+path = '/home/daikinfbn/Brake_Press_Counter4/'
 
-with open('variables.txt','r') as variable_file:
+with open(path + 'variables.txt','r') as variable_file:
     variables = variable_file.readlines()
 
 while variables[0].split('\n')[0] == 'False':
 
-    os.system('python Brake_Press_Counter4.py')
+    os.system('python ' + path + 'Brake_Press_Counter4.py')
 
-    with open('variables.txt','r') as variable_file:
+    with open(path + 'variables.txt','r') as variable_file:
         variables = variable_file.readlines()
 
     if variables[1].split('\n')[0] == 'True':
         os.popen('sudo git stash')
         os.popen('sudo git pull --no-rebase')
-        with open('variables.txt','w') as variable_file:
+        with open(path + 'variables.txt','w') as variable_file:
             variable_file.writelines('False\nFalse')
+            
+with open( path + 'variables.txt','w') as variable_file:
+    variable_file.writelines('False\nFalse')
