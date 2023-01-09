a0729d9f7e095e9196bffea95cd508fd45df40e2
DaikinFBN <109244363+DaikinFBN@users.noreply.github.com>
2023-01-09 13:05:27 -0600

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ while variables[0].split('\n')[0] == 'False':
 
     if variables[1].split('\n')[0] == 'True':
         os.system('sudo git -C ' + path + ' stash ' + secrets.get('GIT_TOKEN'))
-        os.system('sudo git -C ' + path + ' pull ' + secrets.get('GIT_TOKEN') ' --no-rebase')
+        os.system('sudo git -C ' + path + ' pull ' + secrets.get('GIT_TOKEN') + ' --no-rebase')
         os.system('sudo chown daikinfbn ' + path + 'variables.txt')
         with open(path + 'variables.txt','w') as variable_file:
             variable_file.writelines('False\nFalse')
