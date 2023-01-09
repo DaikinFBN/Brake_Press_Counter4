7c58bae774470b3a2117b5133455677c68fe0fc9
DaikinFBN <109244363+DaikinFBN@users.noreply.github.com>
2023-01-09 13:03:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os
+import secrets
 
 path = '/home/daikinfbn/Brake_Press_Counter4/'
 
 os.system('sudo chown daikinfbn ' + path + 'variables.txt')
-
 with open(path + 'variables.txt','r') as variable_file:
     variables = variable_file.readlines()
 
@@ -15,8 +15,8 @@ while variables[0].split('\n')[0] == 'False':
         variables = variable_file.readlines()
 
     if variables[1].split('\n')[0] == 'True':
-        os.system('sudo git -C ' + path + ' stash')
-        os.system('sudo git -C ' + path + ' pull --no-rebase')
+        os.system('sudo git -C ' + path + ' stash' + secrets.get('GIT_TOKEN'))
+        os.system('sudo git -C ' + path + ' pull ' + secrets.get('GIT_TOKEN') ' --no-rebase')
         os.system('sudo chown daikinfbn ' + path + 'variables.txt')
         with open(path + 'variables.txt','w') as variable_file:
             variable_file.writelines('False\nFalse')
