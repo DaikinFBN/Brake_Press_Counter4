2ed49e0cb2900fcbc3629d14fa69262c6b951719
DaikinFBN <109244363+DaikinFBN@users.noreply.github.com>
2023-01-18 13:56:50 -0600

    Update Brake_Press_Counter4.py

--- Brake_Press_Counter4.py
+++ Brake_Press_Counter4.py
@@ -107,8 +107,8 @@ class CounterDisplay:
         self.win.bind('<g>',self.manual_goal_count)
         self.win.bind('<o>',self.open_settings)
 
-        # self.win.geometry("1000x500")
-        self.win.attributes('-fullscreen',True)
+        self.win.geometry("1000x500")
+        # self.win.attributes('-fullscreen',True)
         self.win.grid_columnconfigure(0,weight=1)
         self.win.grid_rowconfigure(0,weight=1)
         self.win.grid_rowconfigure(1,weight=10)
@@ -182,7 +182,7 @@ class CounterDisplay:
         self.undobtn.grid(row=0,column=3,sticky='nwes')
         self.settingsbtn = tk.Button(self.frame_btm,text='Settings',font=fonts[2],fg=font_color[0],bg=bgcolors[1],borderwidth=0,highlightbackground=bgcolors[1],activebackground=bgcolors[2],activeforeground=font_color[0])
         self.settingsbtn.grid(row=0,column=4,sticky='nwes')
-
+        print(variables_data[20])
         if variables_data[20] == 'True': # if the counter is update manually during a shift set the previous bend and shift count to the cached values
             self.bend_count['text'] = variables_data[1]
             self.shift_goal['text'] = variables_data[2]
